8ee3d2a31c078978716e8d0949b032b15cdc889c
DonThePhan <donthephan@gmail.com>
2022-03-22 14:30:00 -0400

    add "read" property to message in server db

--- server/db/models/message.js
+++ server/db/models/message.js
@@ -1,15 +1,23 @@
-const Sequelize = require("sequelize");
-const db = require("../db");
+const Sequelize = require('sequelize');
+const db = require('../db');
 
-const Message = db.define("message", {
-  text: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  senderId: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
+const Message = db.define('message', {
+	text: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	senderId: {
+		type: Sequelize.INTEGER,
+		allowNull: false
+	},
+	read: {
+		type: Sequelize.BOOLEAN,
+		allowNull: false
+	},
+	conversationId: {
+		type: Sequelize.INTEGER,
+		allowNull: false
+	}
 });
 
 module.exports = Message;
